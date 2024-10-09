b1fa405daccd0676fe5c8012b22f297b0cc68bd6
herbert zapata <hzapata@desarrollosocial.gob.cl>
2024-10-08 21:29:12 -0300

    Inicialización del proyecto Laravel

---
+++ database/migrations/2024_10_09_001949_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Nombre del producto
+            $table->text('description')->nullable(); // Descripción opcional
+            $table->decimal('price', 8, 2); // Precio con dos decimales
+            $table->integer('stock'); // Cantidad en inventario
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
